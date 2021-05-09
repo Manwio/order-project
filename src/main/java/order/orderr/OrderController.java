2b74b861236c520af81e0e0d569900885472fe48
Duba Anita <manwio.duba@gmail.com>
2021-05-09 22:37:37 +0200

    Fizetendő érték hozzáadás

--- src/main/java/order/orderr/OrderController.java
+++ src/main/java/order/orderr/OrderController.java
@@ -24,16 +24,47 @@ public class OrderController {
     private Label errorName2;
     @FXML
     private Label errorBase;
+    @FXML
+    private Label errorCheese;
+
 
     @FXML
     TextField name;
     @FXML
     TextField address;
+    @FXML
+    TextField payable;
+
 
     @FXML
     CheckBox base1;
     @FXML
     CheckBox base2;
+    @FXML
+    CheckBox cheese1;
+    @FXML
+    CheckBox cheese2;
+    @FXML
+    CheckBox cheese3;
+    @FXML
+    CheckBox cheese4;
+    @FXML
+    CheckBox topping1;
+    @FXML
+    CheckBox topping2;
+    @FXML
+    CheckBox topping3;
+    @FXML
+    CheckBox topping4;
+    @FXML
+    CheckBox topping5;
+    @FXML
+    CheckBox topping6;
+    @FXML
+    CheckBox topping7;
+    @FXML
+    CheckBox topping8;
+
 
     @FXML
     Text zero;
@@ -47,10 +78,50 @@ public class OrderController {
     @FXML
     Button spinButton;
 
+    public void payableCalculation() {
+        payable.setText(String.valueOf(1000));
+
+        if ((base1.isSelected() && !base2.isSelected()) || (!base1.isSelected() && base2.isSelected())) {
+            payable.setText(String.valueOf(Integer.parseInt(payable.getText()) + 100));
+        }
+
+        if ((cheese1.isSelected() && !cheese2.isSelected() && !cheese3.isSelected() && !cheese4.isSelected())
+            || (!cheese1.isSelected() && cheese2.isSelected() && !cheese3.isSelected() && !cheese4.isSelected())
+            || (!cheese1.isSelected() && !cheese2.isSelected() && cheese3.isSelected() && !cheese4.isSelected())
+            || (!cheese1.isSelected() && !cheese2.isSelected() && !cheese3.isSelected() && cheese4.isSelected())) {
+
+            payable.setText(String.valueOf(Integer.parseInt(payable.getText()) + 100));
+        }
+
+        if (topping1.isSelected()) { payable.setText(String.valueOf(Integer.parseInt(payable.getText()) + 100)); }
+        if (topping2.isSelected()) { payable.setText(String.valueOf(Integer.parseInt(payable.getText()) + 100)); }
+        if (topping3.isSelected()) { payable.setText(String.valueOf(Integer.parseInt(payable.getText()) + 100)); }
+        if (topping4.isSelected()) { payable.setText(String.valueOf(Integer.parseInt(payable.getText()) + 100)); }
+        if (topping5.isSelected()) { payable.setText(String.valueOf(Integer.parseInt(payable.getText()) + 100)); }
+        if (topping6.isSelected()) { payable.setText(String.valueOf(Integer.parseInt(payable.getText()) + 100)); }
+        if (topping7.isSelected()) { payable.setText(String.valueOf(Integer.parseInt(payable.getText()) + 100)); }
+        if (topping8.isSelected()) { payable.setText(String.valueOf(Integer.parseInt(payable.getText()) + 100)); }
+    }
+
+    public void payableAction(ActionEvent event) throws IOException {
+        payableCalculation();
+    }
 
     public void startAction(ActionEvent actionEvent) throws IOException {
         if (base1.isSelected() && base2.isSelected()) {
             errorBase.setText("Csak egy alap választható!");
+        }
+
+        if (!base1.isSelected() && !base2.isSelected()) {
+            errorBase.setText("Válassz egy alapot!");
+        }
+
+        if (cheese1.isSelected() || cheese2.isSelected() || cheese3.isSelected() || cheese4.isSelected()) {
+            errorCheese.setText("Csak egy sajt választható!");
+        }
+
+        if (!cheese1.isSelected() && !cheese2.isSelected() && !cheese3.isSelected() && !cheese4.isSelected()) {
+            errorCheese.setText("Válassz sajtot!");
         }
 
         if (name.getText().isEmpty()) {
@@ -72,6 +143,7 @@ public class OrderController {
 
     private void oneSpin() {
         Text trash = new Text();
+
         trash.setText(zero.getText());
         zero.setText(ten.getText());
         ten.setText(five.getText());
@@ -82,7 +154,7 @@ public class OrderController {
 
     public void spinAction(ActionEvent actionEvent) throws IOException {
         Random rand = new Random();
-        for (int i=0; i < rand.nextInt(19); i++) {
+        for (int i = 0; i < rand.nextInt(19); i++) {
             oneSpin();
         }
 
