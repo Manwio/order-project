5477bdbc9b5edf83dbf11de39ce67fd2406507b2
Duba Anita <manwio.duba@gmail.com>
2021-05-09 21:02:54 +0200

    Spinner kész

--- src/main/java/order/orderr/OrderController.java
+++ src/main/java/order/orderr/OrderController.java
@@ -6,9 +6,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import java.util.Random;
 
 
 import java.io.IOException;
@@ -18,28 +22,71 @@ public class OrderController {
     private Label errorName;
     @FXML
     private Label errorName2;
+    @FXML
+    private Label errorBase;
 
     @FXML
     TextField name;
     @FXML
     TextField address;
 
+    @FXML
+    CheckBox base1;
+    @FXML
+    CheckBox base2;
+
+    @FXML
+    Text zero;
+    @FXML
+    Text three;
+    @FXML
+    Text five;
+    @FXML
+    Text ten;
+
+    @FXML
+    Button spinButton;
+
 
     public void startAction(ActionEvent actionEvent) throws IOException {
-        if (name.getText().isEmpty()) {
-            errorName.setText("ÜRES BAZDMEG!!!!!!!!!!!!!!!!");
-        }
-        if (address.getText().isEmpty()) {
-            errorName2.setText("ÜRES BAZDMEG!!!!!!!!!!!!!!!!");
+        if (base1.isSelected() && base2.isSelected()) {
+            errorBase.setText("Csak egy alap választható!");
         }
 
-        if (!name.getText().isEmpty() && !address.getText().isEmpty()) {
+        if (name.getText().isEmpty()) {
+            errorName.setText("Üres mező!");
+        }
+        if (address.getText().isEmpty()) {
+            errorName2.setText("Üres mező!");
+        }
+
+        if (!name.getText().isEmpty() && !address.getText().isEmpty() &&
+                ((base1.isSelected() && !base2.isSelected()) || (!base1.isSelected() && base2.isSelected()))) {
             Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/successfulorder.fxml"));
             Parent root = fxmlLoader.load();
             stage.setScene(new Scene(root));
             stage.show();
         }
+    }
+
+    private void oneSpin() {
+        Text trash = new Text();
+        trash.setText(zero.getText());
+        zero.setText(ten.getText());
+        ten.setText(five.getText());
+        five.setText(three.getText());
+        three.setText(trash.getText());
+
+    }
+
+    public void spinAction(ActionEvent actionEvent) throws IOException {
+        Random rand = new Random();
+        for (int i=0; i < rand.nextInt(19); i++) {
+            oneSpin();
+        }
+
+        spinButton.setDisable(true);
     }
 
 }
--- src/main/java/order/successful/SuccessfullOrderController.java
+++ src/main/java/order/successful/SuccessfullOrderController.java
@@ -1,6 +1,34 @@
 package order.successful;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
+
 public class SuccessfullOrderController {
 
+    @FXML
+    Button endButton;
+    @FXML
+    Button newButton;
+
+    public void endAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    public void newAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/order.fxml"));
+        Parent root = fxmlLoader.load();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 
 }
