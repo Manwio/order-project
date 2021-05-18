aa56c23a67f4fc82efdd2553a52b2eeaaf0f1d8d
Duba Anita <manwio.duba@gmail.com>
2021-05-18 11:52:14 +0200

    .jar kész

--- src/test/java/order/OrderTest.java
+++ src/test/java/order/OrderTest.java
@@ -1,9 +1,26 @@
 package order;
 
 
-import modell.Pizza;
-
 public class OrderTest {
+    /*
+    CheckBox b1, b2, c1, c2, c3, c4, t1, t2, t3, t4, t5, t6, t7, t8;
+    Text tzero, tthree, tfive, tten, twin;
+    Pizza pizza = new Pizza(b1, b2, c1, c2, c3, c4, t1, t2, t3, t4, t5, t6, t7, t8);
 
-    private Pizza pizza;
+    @BeforeEach
+    void setUp() {
+        tzero.setText("0");
+        tten.setText("10");
+        tfive.setText("5");
+        tthree.setText("3");
+    }
+
+    @Test
+    void testOneSpin() {
+        pizza.spinRandom(2);
+        assertEquals(pizza.percentValue, tfive);
+        assertEquals(pizza.winPercent, tfive);
+    }
+    */
+
 }
